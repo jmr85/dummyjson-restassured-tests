6357dc1845be4234a6e86f5b819b944563797dfa
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-07-25 22:50:54 -0300

    Update AuthTest to expect 201 status code and remove ExampleTest from testNG configuration

--- src/test/java/APITests/AuthTest.java
+++ src/test/java/APITests/AuthTest.java
@@ -43,7 +43,7 @@ public class AuthTest {
                 .when()
                 .post("/auth/login")
                 .then()
-                .statusCode(200)
+                .statusCode(201)// Cambiado a 201 para probar allure
                 .body("accessToken", notNullValue())
                 .body("username", equalTo("oliviaw"));
     }
